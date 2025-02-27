f0218c484c8d69d5b7d49ed801a59f57bc08cd6f
xMarioOP <marioalejandro424@outlook.com>
2025-02-27 15:14:00 -0500

    feat: implement cell interaction logic and dynamic styles

--- src/components/Cell/Cell.tsx
+++ src/components/Cell/Cell.tsx
@@ -1,9 +1,16 @@
 import styles from "./Cell.module.scss";
 
-const Cell = () => {
-    return(
-        <div className={styles.cell}>
-            aaaa
+const Cell = ({ handlePlus, handleMinus, cellClicks, handleMouseDown, handleMouseUp }: any) => {
+    console.log(String(cellClicks.clicks));
+
+    return (
+        <div
+            className={`${styles.cell} ${styles[`level-${String(cellClicks.clicks)}`]}`}
+            onClick={handlePlus}
+            onContextMenu={handleMinus}
+            onMouseDown={handleMouseDown}
+            onMouseUp={handleMouseUp}
+        >
         </div>
     )
 }
